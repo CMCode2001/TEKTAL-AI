d4fb5b36a9d46503f75aecbfa7821e01c368160b
idydiagne <idydiagne1999@gmail.com>
2025-02-27 15:45:08 +0000

    affaire bouy dokh

---
+++ system expert/profils/m.pl
@@ -0,0 +1,9 @@
+% Profil de m
+niveau_etude(m, 1).
+niveau_competence(m, agriculture, 1).
+niveau_competence(m, elevage, 3).
+niveau_matiere(m, svt, 6).
+niveau_matiere(m, frncais, 1).
+niveau_matiere(m, pc, 5).
+interet(m, agronomie, 3).
+trait_personnalite(m, doul, 1).
